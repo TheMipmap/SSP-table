a550640a07321239d2d999c6fc49b3eddfae63eb
Magnus Møller Knudsen <mmkn20@student.aau.dk>
2022-03-25 11:28:48 +0100

    rename of class

---
+++ order_queue.py
@@ -0,0 +1,26 @@
+#from typing_extensions import Self
+from Trivet import trivet
+
+class queue:
+    def __init__(self, trivet_id, user_id):
+        self.trivet_id = trivet_id
+        self.user_id = user_id  
+
+list = []
+msg = "hello word"
+
+def queue_add(self):
+    list.append(Self)
+
+def queue_remove(self):
+    list.pop(Self)
+
+order = queue(4,2)
+queue_add(order)
+
+print(msg)
+
+
+
+
+
